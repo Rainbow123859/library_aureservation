c83814164803ff7269d04e92484659865a3977c9
Rainbow123859 <942451647@qq.com>
2025-09-17 09:03:24 +0800

    更新 zqy.py
    加了250

--- zqy.py
+++ zqy.py
@@ -22,7 +22,7 @@ CONFIG = {
         #{"seatno": "HNND10138", "seatname": "138", "datetime": "510,1320"},
       #  {"seatno": "HNND20480", "seatname": "480", "datetime": "510,1320"},
        # {"seatno": "HNND20482", "seatname": "482", "datetime": "510,1320"},
-        #{"seatno": "HNND20481", "seatname": "481", "datetime": "510,1320"},
+        {"seatno": "HNND04250", "seatname": "250", "datetime": "510,1320"},
         {"seatno": "HNND04252", "seatname": "252", "datetime": "510,1320"},
     ],
     "request_timeout": 5,
