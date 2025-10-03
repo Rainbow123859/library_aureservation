8a90bec0dbbbcd37da60d45fc53166ef2b226a4b
Rainbow123859 <942451647@qq.com>
2025-10-03 23:07:45 +0800

    更新 zqy.py

--- zqy.py
+++ zqy.py
@@ -10,11 +10,11 @@ from requests.adapters import HTTPAdapter
 # ============== 配置区域（根据你的抓包数据修改）===============
 CONFIG = {
     "cookies": {
-        "ASP.NET_SessionId": "kqk4fvfi1b0zvr2aqtnogahv",
+        "ASP.NET_SessionId": "xkm5xaktgizc1gbinrvkhhfa",
         "cookie_unit_name": "%e6%b9%96%e5%8d%97%e5%86%9c%e4%b8%9a%e5%a4%a7%e5%ad%a6%e5%9b%be%e4%b9%a6%e9%a6%86",
         "cookie_come_app": "D935AE54952F16C1",
-        "cookie_come_timestamp": "1758448282",
-        "cookie_come_sno": "DAD084FF07CB0C550DB18A1D0D3C72F183B2F6C5A901BEE1",
+        "cookie_come_timestamp": "1759495491",
+        "cookie_come_sno": "DAD084FF07CB0C55D50B6178B3D2B3A016DA36406D58480C",
         "dt_cookie_user_name_remember": "6C72C7227D4D5EEF6197485F32D9DA8877C790E1EFC9D7A7"
     },
     "seats": [
