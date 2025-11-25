93e2e34aacfdbaaeb1f0e9034efed6719409a4b0
Rainbow123859 <942451647@qq.com>
2025-11-25 22:52:27 +0800

    更新 zqy.py

--- zqy.py
+++ zqy.py
@@ -10,11 +10,11 @@ from requests.adapters import HTTPAdapter
 # ============== 配置区域（根据你的抓包数据修改）===============
 CONFIG = {
     "cookies": {
-        "ASP.NET_SessionId": "kpl1bqr3ik0g4ssiavu3becj",
+        "ASP.NET_SessionId": "zl5kxs2rk4nz4er3qq3tisxe",
         "cookie_unit_name": "%e6%b9%96%e5%8d%97%e5%86%9c%e4%b8%9a%e5%a4%a7%e5%ad%a6%e5%9b%be%e4%b9%a6%e9%a6%86",
         "cookie_come_app": "D935AE54952F16C1",
-        "cookie_come_timestamp": "1763036561",
-        "cookie_come_sno": "DAD084FF07CB0C550A5B0395536E594629DC32A157CF58BF",
+        "cookie_come_timestamp": "1764082230",
+        "cookie_come_sno": "DAD084FF07CB0C5580F6096804E837571694A7B23AF6F07C",
         "dt_cookie_user_name_remember": "6C72C7227D4D5EEF6197485F32D9DA8877C790E1EFC9D7A7"
     },
     "seats": [
@@ -23,7 +23,7 @@ CONFIG = {
       #  {"seatno": "HNND20480", "seatname": "480", "datetime": "510,1320"},
        # {"seatno": "HNND20482", "seatname": "482", "datetime": "510,1320"},
         #{"seatno": "HNND04250", "seatname": "250", "datetime": "510,1320"},
-        {"seatno": "HNND04252", "seatname": "252", "datetime": "450,1320"},
+        {"seatno": "HNND04252", "seatname": "252", "datetime": "480,1320"},
     ],
     "request_timeout": 5,
     "max_attempts": 10  # 每个座位的最大尝试次数
